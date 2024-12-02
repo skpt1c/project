dd53b01162aa9b0843cbdefb92573450e201b858
skpt1c <126434664+skpt1c@users.noreply.github.com>
2024-12-02 09:00:23 +0800

    Delete project directory

--- project/randomloc.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: application/json');
-
-$lat = mt_rand(-900000, 900000) / 10000;
-$lon = mt_rand(-1800000, 1800000) / 10000;
-
-echo json_encode(["lat" => $lat, "lon" => $lon]);
-?>
